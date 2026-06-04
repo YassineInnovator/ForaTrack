cbac34051f21fa99b3f0a94af0dc6faefc6fe8c0
YassineInnovator <elidrissi.yassine.2021.2022@gmail.com>
2026-06-04 11:48:11 +0200

    Création des modèles SQLAlchemy et schémas Pydantic pour Utilisateurs et Chantiers

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:0000@localhost:5432/Fora_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# SessionLocal sera utilisé plus tard pour ouvrir une transaction avec la DB à chaque requête web
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# C'est la classe parente de tous nos futurs modèles
+Base = declarative_base()
